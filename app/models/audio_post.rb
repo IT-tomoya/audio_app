73006db57b8d6ad0e8f033bfb65b74ed093c04be
IT-tomoya <tomoya.maeda.sys@gmail.com>
2019-01-30 14:55:16 +0900

    Add image resize

--- app/models/audio_post.rb
+++ app/models/audio_post.rb
@@ -1,3 +1,13 @@
 class AudioPost < ApplicationRecord
   mount_uploader :picture, PictureUploader
+
+
+  private
+
+    #アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
